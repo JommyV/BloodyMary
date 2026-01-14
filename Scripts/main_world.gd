eb970313985a228d99a021e412756264b0136e09
JommyV <tavaresjmv@gmail.com>
2026-01-14 23:07:17 +0000

    Fixed commit of version 0.2.0.1

--- Scripts/main_world.gd
+++ Scripts/main_world.gd
@@ -14,6 +14,7 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	if day_manager.clients_left == 0:
+		GlobalData.save_data()
 		await get_tree().create_timer(5).timeout
 		get_tree().change_scene_to_file("res://Scenes/UI/end_menu.tscn")
 
